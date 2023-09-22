df04c97774eef962c4b5b7c587c2ff2877ae6e8f
rmalaquias <rafael.malaquias@hotmail.com>
2023-09-21 22:37:23 -0300

    Encapsulando classes, metodos e atributos

--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Principal {
+
+import br.com.alura.screenmatch.models.Movie;
+
+public class Principal {
+    public static void main(String[] args) {
+        Movie meuFilme = new Movie();
+
+        meuFilme.setNome("The Matrix");
+        meuFilme.setAnoDeLancamento(1999);
+        meuFilme.setDuracaoEmMinutos(135);
+        meuFilme.setIncluidoNoPlano(true);
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(9);
+        meuFilme.avalia(8);
+        meuFilme.avalia(9);
+
+        System.out.println(String.format("Média de avaliações do filme: %.2f", meuFilme.pegaMedia()));
+    }
 }
